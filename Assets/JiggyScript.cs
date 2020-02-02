0b0bd66415a585b92b0d8c9669f8fd5e1eb5de82
PhoenixLord1010 <phoenix_lord1010@yahoo.com>
2020-02-01 20:26:02 -0500

    puzzle pieces can be moved

---
+++ Assets/JiggyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JiggyScript : MonoBehaviour
+{
+    private bool move;
+    private Vector3 startPos;
+
+    public Vector3 POS;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+        {
+            transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - startPos.x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - startPos.y, 0);
+        }
+    }
+
+    void Move()
+    {
+        move = true;
+        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    }
+
+    void Drop()
+    {
+        move = false;
+
+    }
+}
---
+++ Assets/PuzzleScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleScript : MonoBehaviour
+{
+    private RaycastHit2D hit;
+    private Touch touch;
+    private Touch[] touches;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        //touch = Input.GetTouch(0);
+        //touches = Input.touches;
+
+        //for (int i = 0; i < Input.touchCount; i++)
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(touches[i].position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hit = Physics2D.Raycast(ray.origin, Vector3.forward);
+            if (hit.collider != null)
+            {
+                hit.collider.SendMessage("Move");
+            }
+        }
+        else if (Input.GetMouseButton(0) && hit.collider != null)
+        {
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            hit.collider.SendMessage("Drop");
+        }
+    }
+}
